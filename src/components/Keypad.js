8809871d03543334375db0e58adf2d8fd9ff7005
anissalopez <anissa965@gmail.com>
2023-06-28 10:02:27 -0400

    event handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from 'react'
 
 function Keypad (){
+    function passwordHandler(){
+        console.log(`Entering password...`)
+    }
     return (
-        <div></div>
+        <div>
+            <input type ="password" onChange ={passwordHandler}></input>
+        </div>
     )
 }
 
